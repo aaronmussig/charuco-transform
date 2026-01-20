dfb9e5b07b9baa012545a9dc7bd389698bfc8b14
aaron <aaronmussig@gmail.com>
2026-01-20 17:51:02 +1000

    update pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
+    compatibilityDate: '2025-07-15',
+    app: {
+        baseURL: '/charuco-transform/'
+    },
+    devtools: {enabled: true},
     telemetry: false,
     ssr: true,
-  modules: ['@nuxt/ui'],
+    modules: ['@nuxt/ui'],
     css: ['~/assets/css/main.css'],
     ui: {
         colorMode: {
